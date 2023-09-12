0a9e63d7b3e4e0b679f6b1614a2cda400dc7892a
Cevij <38700628+Cevij@users.noreply.github.com>
2023-09-12 08:07:15 -0500

    - Added empty comments

--- CodeChallenge/Controllers/IControllers/IEmployeeController.cs
+++ CodeChallenge/Controllers/IControllers/IEmployeeController.cs
@@ -6,11 +6,26 @@ namespace CodeChallenge.Controllers.IController
 {
     public interface IEmployeeController
     {
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
         public IActionResult CreateEmployee([FromBody] Employee employee);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public IActionResult GetEmployeeById(String id);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="newEmployee"></param>
+        /// <returns></returns>
         public IActionResult ReplaceEmployee(String id, [FromBody] Employee newEmployee);
     }
 }
--- CodeChallenge/Data/Contexts/EmployeeContext.cs
+++ CodeChallenge/Data/Contexts/EmployeeContext.cs
@@ -5,11 +5,18 @@ namespace CodeChallenge.Data.Contexts
 {
     public class EmployeeContext : DbContext
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
         public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public DbSet<Employee> Employees { get; set; }
     }
 }
